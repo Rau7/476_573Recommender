77538e350fd7a615b253dd16add9b378f0d36f72
ayca <43960928+aycagrlyk@users.noreply.github.com>
2019-06-30 23:31:06 +0300

    add_reading_book and recommendation functions are added in Profile.php

--- application/controllers/Profile.php
+++ application/controllers/Profile.php
@@ -80,7 +80,65 @@ class Profile extends CI_Controller {
 	
 
 	
+	 public function recommendation($book_id){
+		if(isset($this->session->userdata['admin']['admin_id'])){
 
+
+
+		$data['subview'] = "reccom";
+		$data["title"]= "Öneriler";
+
+		$this->load->model("Book_model");
+		$data['poster']['read']=$this->session->userdata['admin']['admin_name'];
+		$data['poster']['id']=$book_id;
+		$data['poster']['bname']=$this->Book_model->get_book_name($book_id)['book_name'];
+		$data['poster']['bauthor']=$this->Book_model->get_book_author($book_id)['book_author'];
+		$data['poster']['byear']=$this->Book_model->get_book_year($book_id)['book_year'];
+		$data['poster']['bcategory']=$this->Book_model->get_book_category($book_id)['book_genre'];
+
+
+
+
+		   $data['reccs']=$this->Book_model->get_recs($data['poster']);
+
+
+		$this->load->view('layouts/standart',$data);
+
+		}
+		    else{
+		      header("Location: ".base_url());
+		    }
+		}
+
+		public function add_reading_book(){
+		if(isset($this->session->userdata['admin']['admin_id'])){
+		$this->load->model("Book_model");
+		$data['subview'] = "profile";
+		$data["title"]= "Kitap Ekle";
+
+		$books=$this->input->post();
+		$cnt=0;
+		foreach ($books as $key => $value) {
+		if($key!='Ekle'){
+		$data['books'][$cnt]=$key;
+		$cnt++;
+		}
+		}
+		$data['future']=$this->Book_model->get_future_books($this->session->userdata['admin']['admin_name'])[0]['admin_future_id'];
+
+		$cnt2=0;
+		foreach ($data['books'] as $key => $value) {
+		$data['future']=$data['future'].','.$value;
+		}
+
+		$this->Book_model->update_recs($data['future'],$this->session->userdata['admin']['admin_id']);
+
+		$this->load->view('layouts/standart',$data);
+		}
+		    else{
+		      header("Location: ".base_url());
+		    }
+		}
 	
 }
 
