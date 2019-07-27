edf7cdd271a1e420b2d3dbc6079c9aa34543cb20
aycagrlyk <gurleyikayca@gmail.com>
2019-07-27 12:15:06 +0300

    Data Table added

--- application/views/subviews/profile.php
+++ application/views/subviews/profile.php
@@ -25,6 +25,14 @@
                                             <hr class="no_line hrmargin_b_20" />
                                         </div>
                                     </div>
+
+                                    <div class="column one">
+                                        <div class="column_attr" style="text-align: center;">
+                                            <h2 style="margin-bottom: 7px; margin-top: 15px;"><a href="http://localhost/bookstore/index.php/Profile/book_all/">Tüm Kitaplar</a></h2>
+                                            <hr class="no_line hrmargin_b_20" />
+                                        </div>
+                                    </div>
+
                                     <!-- Page devider -->
                                     <!-- One full width row-->
                                     <div class="column one column_divider" style="margin-top: 50px;">
@@ -34,33 +42,35 @@
                                         <div class="column one-second">
                                             <div class="fancy_heading fancy_heading_icon">
                                             <h2 class="title" style="font-size: 40px;">Okunan Kitaplar</h2>
-                                            <?php if(empty($real_reading)){ ?>
+                                            <?php if(empty($real_reading) || $real_reading[0]=="" ){ ?>
                                             <h1 style="margin-top: 50px;"><i>Listelenecek kitap bulunamamıştır</i></h1>
                                             <?php } else{ ?>
-                                            <table class="table table-striped">
-                                                  <thead>
-                                                    <tr>
-                                                      <th scope="col">#</th>
-                                                      <th scope="col">Kitap Adı</th>
-                                                      <th scope="col">Kitap Yazarı</th>
-                                                      <th scope="col">Kitap Türü</th>
-                                                      <th scope="col">Kitap Yılı</th>
-                                                    </tr>
-                                                  </thead>
+
+                                               
+                                            <table id="dtBasicExample" class="table table-striped table-bordered table-sm" cellspacing="0" width="100%">
+                                            <thead>
+                                              <tr>
+                                                <th class="th-sm">#</th>
+                                                <th class="th-sm">Kitap Adı</th>
+                                                <th class="th-sm">Kitap Yazarı</th>
+                                                <th class="th-sm">Kitap Türü</th>
+                                                <th class="th-sm">Rating</th>
+                                              </tr>
+                                            </thead>
                                                   <tbody>
-                                                    <?php $count=1; ?>
+                                                      <?php $count=1; ?>
                                                     <?php foreach ($real_reading as $reads) { ?>
                                                     <tr>
                                                      <th scope="row"><?php echo $count; ?></th>
-                                                      <td><a style="text-decorations:none; color:inherit;" href="http://localhost/bookstore/index.php/Profile/recommendation/<?php echo $reads['book_id']; ?>"><?php echo $reads['book_name']; ?></a></td>
-                                                      <td><a style="text-decorations:none; color:inherit;" href="http://localhost/bookstore/index.php/Profile/recommendation/<?php echo $reads['book_id']; ?>"><?php echo $reads['book_author']; ?></a></td>
-                                                      <td><a style="text-decorations:none; color:inherit;" href="http://localhost/bookstore/index.php/Profile/recommendation/<?php echo $reads['book_id']; ?>"><?php echo $reads['book_genre']; ?></a></td>
-                                                      <td><a style="text-decorations:none; color:inherit;" href="http://localhost/bookstore/index.php/Profile/recommendation/<?php echo $reads['book_id']; ?>"><?php echo $reads['book_year']; ?></a></td>
+                                                      <td><?php echo $reads['book_name']; ?></td>
+                                                      <td><?php echo $reads['book_author']; ?></td>
+                                                      <td><?php echo $reads['book_genre']; ?></td>
+                                                      <td><?php echo $rating[$count-1]; ?></td>
                                                     </tr>
                                                     <?php $count++; ?>
                                                     <?php } ?>
                                                   </tbody>
-                                                </table>
+                                            </table>
                                                 <?php } ?>
                                                 <a href="http://localhost/bookstore/index.php/Profile/add_book_form">
                                                 <button style="margin-top: 15px;">Kitap Ekle</button>
@@ -69,35 +79,42 @@
                                         </div>
                                         <div class="column one-second">
                                             <div class="fancy_heading fancy_heading_icon">
-                                            <h2 class="title" style="font-size: 40px;">Okunacak Kitaplar</h2>
-                                            <?php if(empty($real_future)){ ?>
+                                            <h2 class="title" style="font-size: 40px;">Önerilenler</h2>
+                                            <?php if(empty($reccs) || $reccs[0]=="" ){ ?>
                                             <h1 style="margin-top: 50px;"><i>Listelenecek kitap bulunamamıştır</i></h1>
                                             <?php } else{ ?>
-                                             <form action="<?php echo MAIN; ?>Profile/add_futuretoread_book" method='POST' enctype="multipart/form-data"> 
-                                           <table class="table table-striped">
+                                             <form action="<?php echo MAIN; ?>Profile/add_readed_book" method='POST' enctype="multipart/form-data">
+                                              <table id="dtBasicExample1" class="table table-striped table-bordered table-sm" cellspacing="0" width="100%">
+                                            <thead>
+                                              <tr>
+                                                <th class="th-sm">#</th>
+                                                <th class="th-sm">&nbsp;</th>
+                                                <th class="th-sm">Rating</th>
+                                                <th class="th-sm">Kitap Adı</th>
+                                                <th class="th-sm">Kitap Yazarı</th>
+                                                <th class="th-sm">Kitap Türü</th>
+                                                <th class="th-sm">Benzerlik Skor</th>
+                                              </tr>
+                                            </thead>
+                                                  <tbody>
+                                                     <?php $counter=1; ?>
+                                                      <?php foreach ($reccs as $recc => $value) { ?>
                                                       <tr>
-                                                      <th>&nbsp;</th>
-                                                      <th scope="col">#</th>
-                                                      <th scope="col">Kitap Adı</th>
-                                                      <th scope="col">Kitap Yazarı</th>
-                                                      <th scope="col">Kitap Türü</th>
-                                                      <th scope="col">Kitap Yılı</th>
-                                                      </tr>
-                                                      <?php $counter=1; ?>
-                                                      <?php foreach ($real_future as $recc) { ?>
-                                                      <tr>
-                                                          <td><input type="checkbox" name="<?php echo $recc['book_id']; ?>" /></td>
                                                           <td><?php echo $counter; ?></td>
-                                                          <td><?php echo $recc['book_name']; ?></td>
-                                                          <td><?php echo $recc['book_author']; ?></td>
-                                                          <td><?php echo $recc['book_genre']; ?></td>
-                                                          <td><?php echo $recc['book_year']; ?></td>
+                                                          <td><input type="checkbox" name="<?php echo $value['id']['book_id']; ?>"/></td>
+                                                          <td><input type="number" style="width: 50px;" name="<?php echo $value['id']['book_id']; ?>R"></input></td>
+                                                          <td><?php echo $value['name']; ?></td>
+                                                          <td><?php echo $value['author']['book_author']; ?></td>
+                                                          <td><?php echo $value['genre']['book_genre']; ?></td>
+                                                          <td><?php echo $value['simscore']; ?></td>
                                                       </tr>
                                                       <?php $counter++; ?>
                                                       <?php } ?>
-                                                  </table>
+                                                  </tbody>
+                                            </table>
+
                                                   <br />
-                                                  <!--<input type="submit" name="Ekle" value="Okunanlara Ekle" class="button" style="padding: 3px 6px; font-size: 20px;">-->
+                                                  <input type="submit" name="Ekle" value="Okunanlara Ekle" class="button" style="padding: 3px 6px; font-size: 20px;">
                                                 </form>
                                             <?php } ?>    
                                         </div>
